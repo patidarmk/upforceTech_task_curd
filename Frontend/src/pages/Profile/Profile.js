ccb701d4fd50a1e6947bd57960c2d349617a1809
unknown <patidarmukesh123@gmail.com>
2023-06-15 00:15:30 +0530

    update style

--- Frontend/src/pages/Profile/Profile.js
+++ Frontend/src/pages/Profile/Profile.js
@@ -35,7 +35,7 @@ const Profile = () => {
   return (
     <>
       {
-        showspin ? <Spiner /> : <div className="container">
+        showspin ? <Spiner /> : /* <div className="container">
           <Card className='card-profile shadow col-lg-6 mx-auto mt-5'>
             <Card.Body>
               <Row>
@@ -57,7 +57,84 @@ const Profile = () => {
               </div>
             </Card.Body>
           </Card>
-        </div>
+        </div> */
+		
+		<div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+<div class="col-xl-6 col-md-12">
+                                                <div class="card user-card-full">
+                                                    <div class="row m-l-0 m-r-0">
+                                                        <div class="col-sm-4 bg-c-lite-green user-profile">
+                                                            <div class="card-block text-center text-white">
+                                                                <div class="m-b-25">
+                                                                    <img src={`${BASE_URL}/uploads/${userprofile.profile}`} class="img-radius" alt="User-Profile-Image">
+                                                                </div
+                                                                <h6 class="f-w-600">{userprofile.fname + userprofile.lname}</h6>
+																<p>{user-profile.status}</p>
+                                                                <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="card-block">
+                                                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                                                <div class="row">
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Email</p>
+                                                                        <h6 class="text-muted f-w-400">{userprofile.email}</h6>
+                                                                    </div>
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Phone</p>
+                                                                        <h6 class="text-muted f-w-400">{userprofile.mobile}</h6>
+                                                                    </div>
+																	<div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Gender</p>
+                                                                        <h6 class="text-muted f-w-400">{userprofile.gender}</h6>
+                                                                    </div>
+                                                                </div>
+    
+                                                                <div class="row">
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Location</p>
+                                                                        <h6 class="text-muted f-w-400">{userprofile.location}</h6>
+                                                                    </div>
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Date Created</p>
+                                                                        <h6 class="text-muted f-w-400">{moment(userprofile.datecreated).format("DD-MM-YYYY")}</h6>
+                                                                    </div>
+																	<div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Date Updated</p>
+                                                                        <h6 class="text-muted f-w-400">{userprofile.dateUpdated}</h6>
+                                                                    </div>
+                                                                </div>
+																
+																 <div class="row">
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Recent</p>
+                                                                        <h6 class="text-muted f-w-400">Sam Disuja</h6>
+                                                                    </div>
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Most Viewed</p>
+                                                                        <h6 class="text-muted f-w-400">Dinoter husainm</h6>
+                                                                    </div>
+																	<div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Most Viewed</p>
+                                                                        <h6 class="text-muted f-w-400">Dinoter husainm</h6>
+                                                                    </div>
+                                                                </div>
+																
+																
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                             </div>
+                                                </div>
+                                            </div>
+		
+		
+		
       }
 
     </>
